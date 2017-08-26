59c2af30bce2994d1437a5a0a03444efad6936b4
Aaron Croutch <Aaron@AMA.local>
2017-08-26 04:44:28 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- <link rel="icon" href="../../favicon.ico"> -->
+
+    <title>CSS Grid</title>
+
+    <!-- Custom styles for this template -->
+    <link href="./css/style.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <div class="grid">
+      <div class="t">Header</div>
+      <div class="a">
+        <div class="a-1">
+          <h1>Hello World</h1>
+          <p>This is a test for content</p>
+        </div>
+      </div>
+
+      <div class="b">B</div>
+      <div class="c">Footer</div>
+      <div class="d">D</div>
+    </div>
+
+  
+
+
+  </body>
+
+</html>
